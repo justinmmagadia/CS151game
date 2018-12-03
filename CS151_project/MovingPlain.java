ac58c30f3e3e9ff258c459a97810d5efe95d6fd0
clairelin23 <44530808+clairelin23@users.noreply.github.com>
2018-12-03 00:22:16 -0800

    Update MovingPlain.java

--- CS151_project/MovingPlain.java
+++ CS151_project/MovingPlain.java
@@ -1,4 +1,38 @@
-public MovingPlain
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.Icon;
+
+public class MovingPlain implements Icon
 {
-    
+	int width;
+	int height;
+	private MoveableShape shape;
+	public MovingPlain(MoveableShape shape)
+	{
+		this.shape = shape;
+		this.width = shape.width();
+		this.height =shape.height();
+	}
+	@Override
+	public int getIconHeight() 
+	{
+		
+		return height;
+	}
+
+	@Override
+	public int getIconWidth() 
+	{
+		return width;
+	}
+
+	@Override
+	public void paintIcon(Component arg0, Graphics g, int arg2, int arg3) 
+	{
+		Graphics2D g2 = (Graphics2D) g;
+	    shape.draw(g2);	
+	}
+
 }
